84ca9423f9e2669f9144c84c319f6de308ac1011
Jihyo Kang <luxuryjihyo@naver.com>
2022-04-15 09:31:44 +0900

    15 Apr 2022

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,25 +1,72 @@
+/* esLint disable */
+import React, {useState} from 'react';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl  } from 'react-bootstrap';
+
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
+  let [shoes,shoes변경] = useState(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">신발가게</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className='background'>
+        <h1>20% Season Off</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map((a, i)=>{
+              return (
+                <ShoeList data={shoes[i]} i={i} key={i} />
+              )
+            })
+          }
+
+        </div>
+      </div>
+
     </div>
   );
+
+  function ShoeList(props){
+
+    return(
+      <div className="col-md-4">
+        <img src={"https://codingapple1.github.io/shop/shoes"+(props.i+1)+".jpg"} width="100%" />
+        <h4>{props.data.title}</h4>
+        <p>{props.data.content} & {props.data.price}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
